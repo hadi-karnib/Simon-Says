740b02bb5218d57599b072e54534f17792975c42
karniib <hadikarnib03@gmail.com>
2024-06-30 07:54:33 +0300

    fixed formatting and  syntax errors

--- game.js
+++ game.js
@@ -1,86 +1,82 @@
-const buttonColors = ["red", "blue", "green", "yellow"]
+const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-let userClickedPattern = []
-let started = false
-let level = 0
+let userClickedPattern = [];
+let started = false;
+let level = 0;
 
-document.addEventListener('keypress', () => {
-
-    if (!started) {
-        document.querySelector("#level-title").textContent = `Level ${level}`
-        nextSequence();
-        started = true
-    }
-})
+document.addEventListener("keypress", () => {
+  if (!started) {
+    document.querySelector("#level-title").textContent = `Level ${level}`;
+    nextSequence();
+    started = true;
+  }
+});
 function flash(color) {
-    const button = document.getElementById(color);
-    button.classList.add("pressed");
-    setTimeout(() => {
-        button.classList.remove("pressed");
-    }, 200);
+  const button = document.getElementById(color);
+  button.classList.add("pressed");
+  setTimeout(() => {
+    button.classList.remove("pressed");
+  }, 200);
 }
 
 function playSound(name) {
-    let audio = new Audio(sounds/${name}.mp3);
-    audio.play();
+  let audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
 }
 
-document.querySelectorAll(".btn").forEach(btn => {
-    btn.addEventListener('click', function() {
-        let userChosenColor = this.id;
-        userClickedPattern.push(userChosenColor);
+document.querySelectorAll(".btn").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
 
-        playSound(userChosenColor);
-        animations(userChosenColor);
+    playSound(userChosenColor);
+    animations(userChosenColor);
 
-        checkAnswer(userClickedPattern.length - 1);
-    });
+    checkAnswer(userClickedPattern.length - 1);
+  });
 });
 
 function nextSequence() {
-    userClickedPattern = [];
-    level++;
-    document.querySelector("#level-title").textContent = Level ${level};
+  userClickedPattern = [];
+  level++;
+  document.querySelector("#level-title").textContent = `Level ${level}`;
 
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
 
-    flash(randomChosenColor);
+  flash(randomChosenColor);
 
-    playSound(randomChosenColor);
+  playSound(randomChosenColor);
 }
 
 function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        if (userClickedPattern.length === gamePattern.length){
-            setTimeout(() => {
-                nextSequence();
-            }, 1000);
-        }
-    } else {
-        playSound("wrong");
-        document.querySelector("body").classList.add("game-over");
-        document.querySelector("#level-title").textContent = "Game Over, Press Any Key to Restart";
-        setTimeout(() => {
-            document.querySelector("body").classList.remove("game-over");
-        }, 200);
-        reRun();
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
     }
+  } else {
+    playSound("wrong");
+    document.querySelector("body").classList.add("game-over");
+    document.querySelector("#level-title").textContent =
+      "Game Over, Press Any Key to Restart";
+    setTimeout(() => {
+      document.querySelector("body").classList.remove("game-over");
+    }, 200);
+    reRun();
+  }
 }
 
 function animations(currentColor) {
-    let button = document.getElementById(currentColor);
-    button.classList.add("pressed");
-    setTimeout(() => button.classList.remove("pressed"), 100);
+  let button = document.getElementById(currentColor);
+  button.classList.add("pressed");
+  setTimeout(() => button.classList.remove("pressed"), 100);
 }
 
 function reRun() {
-    level = 0;
-    gamePattern = [];
-    started = false;
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
-
-
-
-
